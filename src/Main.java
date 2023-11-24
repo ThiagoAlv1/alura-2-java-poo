e5730d9b4125019ddf1520271cbec698fa6186be
Thiago <thiago___melo@hotmail.com>
2023-11-24 19:08:07 -0300

    atributos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Filme meuFilme = new Filme();
+
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoLancamento = 1970;
+        meuFilme.duracaoEmMinuto = 180;
+
+        System.out.println(meuFilme.nome);
+        System.out.println(meuFilme.anoLancamento);
     }
 }
